c241a9c90004857255467dd2984483ed68cd79cb
liushuai <liushuai@qq.com>
2019-05-26 22:19:55 +0800

    修改变量名

--- redux/combineReducers.js
+++ redux/combineReducers.js
@@ -2,6 +2,6 @@
  * 合并reducers模块
  * by liushuai
  */
-export default reducers => (state = {}, action) => Object.keys(reducers).reduce((obj, reducerKey) => ((obj[reducerKey] = reducers[reducerKey](state[reducerKey], action)), obj),{})
+export default reducers => (state = {}, action) => Object.keys(reducers).reduce((memo, reducerKey) => ((memo[reducerKey] = reducers[reducerKey](state[reducerKey], action)), memo),{})
 
 
