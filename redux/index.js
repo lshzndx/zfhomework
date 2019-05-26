44a8fae3241d6c61a7c67536a4518023ad47de44
liushuai <liushuai@qq.com>
2019-05-26 21:37:11 +0800

    导出bindActionCreators方法

--- redux/index.js
+++ redux/index.js
@@ -6,8 +6,10 @@
 import createStore from './createStore'
 import combineReducers from './combineReducers'
 import applyMiddleware from './applyMiddleware'
+import bindActionCreators from './bindActionCreators'
 export {
   createStore,
   combineReducers,
-  applyMiddleware
+  applyMiddleware,
+  bindActionCreators
 }
