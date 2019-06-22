aa2060dd6210ba45e3dceb87ceb3633045130eda
liushuai <3064173938@qq.com>
2019-06-22 19:09:15 +0800

    dva初版实现

---
+++ dva/index.js
@@ -0,0 +1,51 @@
+/**
+ * dva核心实现
+ * by liushuai
+ */
+import React from 'react'
+import reactDom from 'react-dom'
+import createSagaMiddleware from 'redux-saga'
+import * as effects from 'redux/saga/effects'
+import { createStore, combineReducers, Provider } from 'redux'
+import reactDom from 'react-dom'
+const dva = () => {
+  let CurrentRootComponent
+  const currentModels = []
+
+  const model = model => currentModels.push(model)
+
+  const router = fnComponent => CurrentRootComponent = fnComponent
+
+  const start = root => {
+    const reducers = currentModels.reduce((reducer, model) => {
+      const {namespace, reducers, state} = model
+      const [, actionType] = action.type.split('/')
+      reducer[namespace] = (state = state, action) => typeof reducers[action.type] === 'function' ? reducers[action.type](state, actionType) : state
+      return reducer
+    }, {})
+
+    function* rootSaga () {
+      for(const model of currentModels) {
+        const {namespace, effects: currentEffects = {}} = model
+        for (const key in currentEffects) {
+          yield effects.takeEvery(`${namespace}/${key}`, currentEffects[key], effects)
+        }
+      }
+    }
+
+    const sagaMiddleware = createSagaMiddleware()
+    const reducer = combineReducers(reducers)
+
+    const store = createStore(sagaMiddleware)(reducer)
+
+    sagaMiddleware.run(rootSaga)
+
+    reactDom.render(
+      <Provider store={store}><CurrentRootComponent /></Provider>, document.querySelector(root)
+    )
+  }
+
+  return {model, router, start}
+}
+
+export default dva
