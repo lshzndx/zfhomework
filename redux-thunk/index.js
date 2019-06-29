ea80be9b82828e2ca310f2c9ea76fea36096018e
liushuai <liushuai@qq.com>
2019-06-29 15:38:01 +0800

    redux-thunk实现

---
+++ redux-thunk/index.js
@@ -0,0 +1,13 @@
+/**
+ * redux-thunk核心实现
+ * by liushuai
+ */
+const ReduxThunk = store => dispatch => action => {
+  if (typeof action === 'function') action(dispatch, store.getState, ReduxThunk.api)
+  else if (typeof action.then === 'function') action.then(dispatch)
+  else dispatch(action)
+}
+
+ReduxThunk.withExtraArgument = api => (ReduxThunk.api = api, ReduxThunk)
+
+export default ReduxThunk
